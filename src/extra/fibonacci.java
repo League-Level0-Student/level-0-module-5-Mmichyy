fcadb91b4157924020ee37c186338d4defeb8c85
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-28 10:57:22 -0700

    fibonacci,GuessingPi

--- src/extra/fibonacci.java
+++ src/extra/fibonacci.java
@@ -4,14 +4,26 @@ public class fibonacci {
 
 public static void main(String[] args) {
 
-int a=
-int b=
+int one= 1;
+int two= 1;
+int three;
+
+System.out.println(one);
+System.out.println(two);
+
+for (int i=0;i < 13; i++) {
 	
-for (int i=0;i < 13; i++);
+three=one+two;
+
+System.out.println(three);
+
+one=two;
+
+two=three;
 	
-System.out.println();
 
 }	
+}}
 
-}	
+//1+1=2+1=3+2=5+3=8=13
 
