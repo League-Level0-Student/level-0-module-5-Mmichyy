deb37fc393648ac2518599f5f0ed14410329173b
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-18 10:46:27 -0700

    guessing pi

--- src/GuessingPi.java
+++ src/GuessingPi.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 0
@@ -11,39 +12,41 @@ public static void main(String[] args) {
 
 		// 2. Make a String variable to hold the value of Pi.
 		//    Get the first few digits from http://www.piday.org/million/.
-String Pi= "3.1415926";
+String pi= "3.1415926";
 
 // 3. Print out the first 3 digits of Pi to the console.
-System.out.println(3.14);	
+	//System.out.printIn(value.charAt(0));
+    //System.out.printIn(value.charAt(1));
+    //System.out.printIn(value.charAt(28));
+
 //    The first value is "pi.charAt(0)", the second is "pi.charAt(1)", etc.
 		//    Run your program to see how this works.
-Pi.charAt(0);
-Pi.charAt(1);		
-Pi.charAt(2);
+pi.charAt(0);
+pi.charAt(1);		
+pi.charAt(2);
 		// 9. If you want to give the user more than one chance to guess,
 		//    put a for loop around steps 4-8.
-for(int i=4;i<8;i++) {
-yourStringVariable.length(8);
+for(int i=0;i<100;i++) {
+
+for (int i1 = 0; i1 < pi.length();i1++) {
+System.out.println(pi.charAt(i1));
 
 	// 4. Create a for loop that will step through each digit of pi (steps 5-8)
 			//    NOTE: The number of digits in your string will be
 			//       yourStringVariable.length()
 
 				// 5. Ask the user for the NEXT digit of pi.
-JOptionPane.showMessageDialog(null, "What is the next didgit ogf Pi?");
-				// 6. Compare the user's input to the next digit of your pi variable 
-}				//    (look at step 3 for a clue).
-}				//    HINT: use charAt(0) to get 1st char of user input String 
+String guess =JOptionPane.showInputDialog(null, "What is the next digit of Pi?");
+if (guess.equals(pi.charAt(i1)+"")){
+JOptionPane.showMessageDialog(null,"correct");}
+// 6. Compare the user's input to the next digit of your pi variable 
+			
+//    (look at step 3 for a clue).
+				//    HINT: use charAt(0) to get 1st char of user input String 
 
-					// 7. If they are correct, print out "correct".
-System.out.println(correct);
+else {JOptionPane.showMessageDialog(null,"incorrect");
+break;
 
-
-
-}
-					// 8. If they are not, print out "incorrect" and tell them
-					//    to start over. Use 'break;' to break out of the loop.
-
-}
-orrect
-
+// 8. If they are not, print out "incorrect" and tell them
+// to start over. Use 'break;' to break out of the loop.
+}}}}}
