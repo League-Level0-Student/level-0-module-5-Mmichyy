11c58b0ea417e07929a40a9f07fe6575a5c8adfc
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-08 10:46:28 -0700

    loppp

--- src/extra/loppp.java
+++ src/extra/loppp.java
@@ -3,21 +3,67 @@ package extra;
 public class loppp {
 public static void main(String[] args) {
 
-for(int i=0;i<100;i++) {
-	System.out.println(i);
-	
-for(int i=2;i<101;i++) {
-	System.out.println();
-	
-for(int i=1;i<100;i++) {	
-	System.out.println();
-	
-for(int i=1;i<501;i++) {
-	System.out.println();
+//for(int i=0;i<100;i++) {
+//	System.out.println(i);
+//}	
+//for(int i=2;i<101;i++) {
+//	System.out.println();
+//}	
+//for(int i=1;i<100;i++) {	
+//	System.out.println();
+//}	
+//for(int i=1;i<501;i++) {
+//	if(i%2==0) {
+//	System.out.println(i+"is even");
+//}
+//else {
+//	System.out.println(i+"is odd");
+//}
+//for int j=0;j<784;j+=7){
+//	System.out.println(j);
+//}
+//int age=0;
+//for(int i=2002; i< 2019;i++) {
+//	System.out.println(i+"I was"+age);
+//age++;
+//}
+//
+//for(int j=0;j<7;j+=3){
+//for(int i=1;i<4;i++) {
+//	System.out.print(i+j);
+//}
+//System.out.println();
+//}
+//
+//
+//for(int i=0; i<91;i+=10) {
+//	for(int j=1;j<11;j++) {
+//
+//System.out.print(i+j);
+//	}
+//	System.out.println();
+
+//}
+
+for(int i=0; i<1; i+=6) {
+for(int j=1;j<2;j++) {
+System.out.println("*");
 
 
-	
-	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	
 	
 	
